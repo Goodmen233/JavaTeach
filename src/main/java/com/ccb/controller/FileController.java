d8221cb1fe8b6d5ef76520954efa41bc99e3dea4
Goodmen <841306289@qq.com>
2023-05-16 20:39:21 +0800

    feature: 新增文件口

--- src/main/java/com/ccb/controller/FileController.java
+++ src/main/java/com/ccb/controller/FileController.java
@@ -2,21 +2,26 @@ package com.ccb.controller;
 
 import cn.hutool.core.bean.BeanUtil;
 import com.ccb.common.urls.FileUrl;
+import com.ccb.common.utils.ImageUtil;
 import com.ccb.common.utils.OssUtil;
 import com.ccb.domain.common.ResultInfo;
 import com.ccb.domain.po.FilePO;
+import com.ccb.domain.vo.req.file.CkUploadReq;
 import com.ccb.domain.vo.req.file.DeleteReq;
 import com.ccb.domain.vo.req.file.UploadReq;
 import com.ccb.service.FileService;
+import com.google.common.collect.Maps;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.multipart.MultipartFile;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 /**
  * @Description: 文件相关接口
@@ -51,8 +56,12 @@ public class FileController {
     @ApiOperation("文件图片上传-new")
     @PostMapping(FileUrl.IMG_UPLOAD)
     @ResponseBody
-    public ResultInfo<String> imageUpload(@RequestPart("upload")MultipartFile file) {
-        return ResultInfo.success(OssUtil.uploadOneFile(file));
+    public Map<String, Object> imageUpload(CkUploadReq ckUploadReq) {
+        HashMap<String, Object> map = Maps.newHashMap();
+        map.put("uploaded", 1);
+        map.put("url", OssUtil.uploadOneFile(ImageUtil.base64ToMultipartFile(ckUploadReq.getUpload())));
+        map.put("fileName", UUID.randomUUID());
+        return map;
     }
 
 }
